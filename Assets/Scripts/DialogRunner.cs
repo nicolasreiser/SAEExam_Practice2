3afa9e635b2c1826f261c09fd344f8eafc1ae85e
Luca Martinelli <lucxmangajet@gmail.com>
2021-04-09 13:47:24 +0200

    added sprites, prepared for practice

--- Assets/Scripts/DialogRunner.cs
+++ Assets/Scripts/DialogRunner.cs
@@ -2,16 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestDialogExample : MonoBehaviour
+public class DialogRunner : MonoBehaviour
 {
     [SerializeField] DialogDisplayer dialogDisplayer;
 
     private void Start()
     {
-        StartCoroutine(DialogRoutine());
+        StartCoroutine(DialogRoutineTest());
     }
 
-    private IEnumerator DialogRoutine()
+    private IEnumerator DialogRoutineTest()
     {
         Debug.Log("Running Test");
         yield return new WaitForSeconds(1);
@@ -21,22 +21,22 @@ public class TestDialogExample : MonoBehaviour
         yield return new WaitForSeconds(3);
         dialogDisplayer.SetText("Beat.");
 
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(2);
         dialogDisplayer.SetInterlocutor(Interlocutor.Character1);
         dialogDisplayer.SetName("JOKER");
         dialogDisplayer.SetText("is it just me, or is it getting crazier out there?");
 
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(4);
         dialogDisplayer.SetInterlocutor(Interlocutor.None);
         dialogDisplayer.SetText("Despite the laughter, there's real pain in his eyes. Something broken in him.Looks like he hasn't slept in days.");
 
-        yield return new WaitForSeconds(4);
+        yield return new WaitForSeconds(5);
 
         dialogDisplayer.SetInterlocutor(Interlocutor.Character2);
         dialogDisplayer.SetName("SOCIAL WORKER");
         dialogDisplayer.SetText("It's certainly tense. People are upset, they're struggling. Looking for work.The garbage strike seems like it's been going on forever.These are tough times.");
 
-        yield return new WaitForSeconds(7);
+        yield return new WaitForSeconds(10);
         dialogDisplayer.SetInterlocutor(Interlocutor.None);
         dialogDisplayer.SetText("");
     }
